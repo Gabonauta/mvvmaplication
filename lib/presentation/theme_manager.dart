553ea2c83139a4f0f24fede331a451957e6cd8d0
Gabonauta <dante9825@gmail.com>
2023-02-02 12:07:27 -0500

    add value s4 and card theme values

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 
 import 'color_manager.dart';
+import 'values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main colors of the app
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.primaryOpacity70,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager
-          .grey1, // will be used incase of disabled button for example
-      secondaryHeaderColor: ColorManager.grey
-      // card view theme
+    // main colors of the app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager
+        .grey1, // will be used incase of disabled button for example
+    secondaryHeaderColor: ColorManager.grey,
+    // card view theme
+    cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4),
+    // App bar theme
 
-      // App bar theme
+    // Button theme
 
-      // Button theme
+    // Text theme
 
-      // Text theme
-
-      // input decoration theme (text form field)
-
-      );
+    // input decoration theme (text form field)
+  );
 }
