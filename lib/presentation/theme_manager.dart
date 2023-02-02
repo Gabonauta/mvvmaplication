de8c05d0981f68da30d983962665474833723c37
Gabonauta <dante9825@gmail.com>
2023-02-02 12:24:42 -0500

    button and elevated button theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -7,35 +7,51 @@ import 'values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main colors of the app
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.primaryOpacity70,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager
-          .grey1, // will be used incase of disabled button for example
+    // main colors of the app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager
+        .grey1, // will be used incase of disabled button for example
+    splashColor: ColorManager.primaryOpacity70,
+    secondaryHeaderColor: ColorManager.grey,
+    // card view theme
+    cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4),
+    // App bar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
+        color: ColorManager.white,
+        fontSize: FontSize.s16,
+      ),
+    ),
+
+    // Button theme
+    buttonTheme: ButtonThemeData(
+      shape: const StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
       splashColor: ColorManager.primaryOpacity70,
-      secondaryHeaderColor: ColorManager.grey,
-      // card view theme
-      cardTheme: CardTheme(
-          color: ColorManager.white,
-          shadowColor: ColorManager.grey,
-          elevation: AppSize.s4),
-      // App bar theme
-      appBarTheme: AppBarTheme(
-        centerTitle: true,
-        color: ColorManager.primary,
-        elevation: AppSize.s4,
-        shadowColor: ColorManager.primaryOpacity70,
-        titleTextStyle: getRegularStyle(
-          color: ColorManager.white,
-          fontSize: FontSize.s16,
+    ),
+    //elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(color: ColorManager.white),
+        backgroundColor: ColorManager.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
         ),
-      )
+      ),
+    ),
 
-      // Button theme
+    // Text theme
 
-      // Text theme
-
-      // input decoration theme (text form field)
-      );
+    // input decoration theme (text form field)
+  );
 }
