19478ef836e2a2bd3f4f324afe78a9fb0f011996
Gabonauta <dante9825@gmail.com>
2023-02-02 12:18:02 -0500

    Add appbartheme params

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,28 +1,41 @@
 import 'package:flutter/material.dart';
 
 import 'color_manager.dart';
+import 'font_manager.dart';
+import 'styles_manager.dart';
 import 'values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    // main colors of the app
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.primaryOpacity70,
-    primaryColorDark: ColorManager.darkPrimary,
-    disabledColor: ColorManager
-        .grey1, // will be used incase of disabled button for example
-    secondaryHeaderColor: ColorManager.grey,
-    // card view theme
-    cardTheme: CardTheme(
-        color: ColorManager.white,
-        shadowColor: ColorManager.grey,
-        elevation: AppSize.s4),
-    // App bar theme
+      // main colors of the app
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primaryOpacity70,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager
+          .grey1, // will be used incase of disabled button for example
+      splashColor: ColorManager.primaryOpacity70,
+      secondaryHeaderColor: ColorManager.grey,
+      // card view theme
+      cardTheme: CardTheme(
+          color: ColorManager.white,
+          shadowColor: ColorManager.grey,
+          elevation: AppSize.s4),
+      // App bar theme
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.primaryOpacity70,
+        titleTextStyle: getRegularStyle(
+          color: ColorManager.white,
+          fontSize: FontSize.s16,
+        ),
+      )
 
-    // Button theme
+      // Button theme
 
-    // Text theme
+      // Text theme
 
-    // input decoration theme (text form field)
-  );
+      // input decoration theme (text form field)
+      );
 }
