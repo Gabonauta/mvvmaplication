ef8ad6e207acbc8047662e732558e1b272b233ce
Gabonauta <dante9825@gmail.com>
2023-02-07 12:26:41 -0500

    add presentation views, reestruture presentation files, route manager strings

---
+++ lib/presentation/store_details/store_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StoreDetails extends StatelessWidget {
+  const StoreDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
